b1a73cecca84ee67ce01d008dd725f463abfa3b9
Neil Shepperd <nshepperd@gmail.com>
2014-01-15 02:09:39 +1100

    Allow score to depend on actions as well as state

--- vacuum/sim.hs
+++ vacuum/sim.hs
@@ -6,29 +6,30 @@ data Direction = R | U | L | D deriving (Show, Eq)
 data Action = Go Direction | Suck deriving (Show, Eq)
 
 type MapFunc a = a -> Direction -> a
-data State a b = State { getLocation :: a, getDirt :: (Set a), getMem :: b } deriving (Show, Eq)
+data State a b = State { getLocation :: a, getDirt :: (Set a), getMem :: b, getScore :: Int } deriving (Show, Eq)
 
 data SimpleWorld = LeftSquare | RightSquare deriving (Show, Eq, Ord)
 
-data Strategy a b = SWM { decide :: b -> a -> Bool -> (Action, b),
-                          initialmem :: b }
+data Strategy a b = Strategy { decide :: b -> a -> Bool -> (Action, b),
+                               initialmem :: b }
+
+data Environment a b = Environment { godir :: (MapFunc a),
+                                      scoreState :: (State a b) -> Int,
+                                      scoreAction :: Action -> Int }
 
 statelessStrategy :: (a -> Bool -> Action) -> (Strategy a ())
-statelessStrategy decide = SWM decide2 ()
+statelessStrategy decide = Strategy decide2 ()
   where decide2 () loc dirt = (decide loc dirt, ())
 
 
-stepState :: (Ord a) => (MapFunc a) -> State a b -> (Strategy a b) -> State a b
-stepState godir (State location dirt mem) strategy =
+stepState :: (Ord a) => Environment a b -> State a b -> (Strategy a b) -> State a b
+stepState env state@(State location dirt mem score) strategy =
   case decide strategy mem location (Set.member location dirt) of
-    (Suck, newmem) -> State location (Set.delete location dirt) newmem
-    (Go d, newmem) -> State (godir location d) dirt newmem
+    (Suck, newmem) -> State location (Set.delete location dirt) newmem (score + scoreState env state + scoreAction env Suck)
+    (Go d, newmem) -> State (godir env location d) dirt newmem (score + scoreState env state + scoreAction env (Go d))
 
-evaluate :: State a b -> Int
-evaluate (State _ dirt _) = -(Set.size dirt)
-
-simulate :: (Ord a) => (MapFunc a) -> (State a b) -> (Strategy a b) -> Int
-simulate godir initial strategy = snd $ (iterate (\(state, score) -> (stepState godir state strategy, score + evaluate state)) (initial, 0)) !! 1000
+simulate :: (Ord a) => Environment a b -> (State a b) -> (Strategy a b) -> Int
+simulate env initial strategy = getScore $ (iterate (\state -> stepState env state strategy) initial) !! 1000
 
 -- utilities
 powerList :: [a] -> [[a]]
@@ -51,6 +52,14 @@ reflexStrategy _ True = Suck
 reflexStrategy LeftSquare _ = Go R
 reflexStrategy RightSquare _ = Go L
 
+memDecide :: (Set SimpleWorld) -> SimpleWorld -> Bool -> (Action, (Set SimpleWorld))
+memDecide cleaned location True = (Suck, Set.insert location cleaned)
+memDecide cleaned location@LeftSquare False = (if Set.member RightSquare cleaned then Suck else Go R, Set.insert location cleaned)
+memDecide cleaned location@RightSquare False = (if Set.member LeftSquare cleaned then Suck else Go L, Set.insert location cleaned)
+
+memStrategy :: Strategy SimpleWorld (Set SimpleWorld)
+memStrategy = Strategy { decide = memDecide, initialmem = Set.empty }
+
 -- unknown map world
 minimap :: [String]
 minimap = [
@@ -67,6 +76,10 @@ unknownStrategy :: (Int,Int) -> Bool -> Action
 unknownStrategy _ True = Suck
 unknownStrategy _ False = Go R
 
+penalizeMotion :: Action -> Int
+penalizeMotion (Go _) = -1
+penalizeMotion Suck = 0
+
 main :: IO ()
 main = do
   -- let strat = statelessStrategy unknownStrategy
@@ -77,11 +90,14 @@ main = do
   --       let initial = (State loc (Set.fromList dirt) (initialmem strat))
   --       print initial
   --       print (simulate miniGo initial strat))
-  let strat = statelessStrategy reflexStrategy
+  let env = Environment { godir = simpleGo,
+                          scoreState = (\state -> -(Set.size (getDirt state))),
+                          scoreAction = penalizeMotion }
+  let strat = memStrategy --statelessStrategy reflexStrategy
   let squares = [LeftSquare, RightSquare]
   forM_
     (cartesian (powerList squares) squares)
     (\(dirt, loc) -> do
-        let initial = (State loc (Set.fromList dirt) (initialmem strat))
+        let initial = (State loc (Set.fromList dirt) (initialmem strat) 0)
         print initial
-        print (simulate simpleGo initial strat))
+        print (simulate env initial strat))
